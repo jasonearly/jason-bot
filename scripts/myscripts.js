7d0cc84d9d6f8606ea40ba5b5e40d97450b79692
Jason Early <theoffice@gruntmonkey.com>
2016-04-30 12:26:24 -0500

    code cleanup

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -2,19 +2,19 @@ module.exports = function(robot) {
 
 	// robot.hear() will have the robot listen in on any channel it's in
 	robot.hear(/badger/i, function(response) {
-		response.send("Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS")
-	})
+		response.send("Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS");
+	});
 
 	robot.hear(/i dislike (.+)/i, function(response) {
-		var like = response.match[1]
+		var like = response.match[1];
 		// response.reply() will have the robot at-mention you when it responds
-		response.reply('I also dislike ' + like)
-	})
+		response.reply('I also dislike ' + like);
+	});
 
 	// robot.respond() means the robot will only reply when at-mentioned or DMed
 	robot.respond(/who should i call on today\?/i, function(response) {
-		var user = robot.brain.userForName('gordo')
-		response.reply('Call on ' + user.real_name + '!')
-	})
+		var user = robot.brain.userForName('gordo');
+		response.reply('Call on ' + user.real_name + '!');
+	});
 
-}
+};
