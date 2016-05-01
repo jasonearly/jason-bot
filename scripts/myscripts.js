70561c9f1235281a740d0fcea083b25605721961
Jason Early <theoffice@gruntmonkey.com>
2016-05-01 12:18:18 -0500

    image reaction

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -64,10 +64,8 @@ module.exports = function(robot) {
 	var reactions;
 
 	reactions = [
-	'http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg',
-	'https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png',
-	'http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png',
-	'http://shipitsquirrel.github.com/images/squirrel.png'
+	'http://i.giphy.com/l3L7Yujvb5gC7MjFm.gif',
+
 	];
 
 	robot.hear(/celebrate/i, function(msg) {
