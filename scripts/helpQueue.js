d4f0b54f2ae1cef7a8e2eafb9632acf1e6e4f896
Jason Early <theoffice@gruntmonkey.com>
2016-05-02 10:18:14 -0500

    helpqueue

--- scripts/helpQueue.js
+++ scripts/helpQueue.js
@@ -35,7 +35,7 @@ module.exports = function(robot) {
 
 	robot.respond(/mentionname is (.+)/i, function(msg) {
 		//var mentionname, user;
-		var mentionName = msg.match[2];
+		var mentionName = msg.match[1];
 		//user = robot.brain.userForId(msg.envelope.user.id);
 		//return user.mentionname = mentionname;
 		response.reply('Call on ' + mentionName + '!');
