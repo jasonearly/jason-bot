f423b5b82c87edf77018ce9f2d46f2513345c449
Jason Early <theoffice@gruntmonkey.com>
2016-05-02 19:38:18 -0500

    helpqueue

--- scripts/helpQueue.js
+++ scripts/helpQueue.js
@@ -35,7 +35,7 @@ module.exports = function(robot) {
   robot.respond(/who is @?([\w .\-]+)\?*$/i, function(response) {
     var name, user, users;
     name = response.match[1].trim();
-    users = robot.brain.usersForFuzzyName(name);
+    users = robot.brain.usersForName(name);
     if (users.length === 1) {
       user = users[0];
 //do something here
